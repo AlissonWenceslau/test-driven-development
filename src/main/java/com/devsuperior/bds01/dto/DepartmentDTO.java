bee357cd3dc1c31afbf29b87307c42535d330adc
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-05-05 21:35:20 -0300

    First commit

---
+++ src/main/java/com/devsuperior/bds01/dto/DepartmentDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.bds01.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.bds01.entities.Department;
+
+public class DepartmentDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	public Long id;
+	public String name;
+	
+	public DepartmentDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public DepartmentDTO(Department entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+}
